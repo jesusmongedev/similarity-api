d25eab63eebbe79fa02a2bb364c3ed42e31a83b2
Jesús Monge <jesus_jaml@outlook.com>
2023-04-07 15:08:41 -0500

    feat: update imports + core layout styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Heading from "./components/ui/LargeHeading";
-import Paragraph from "./components/ui/Paragraph";
+import Heading from "@/ui/LargeHeading";
+import Paragraph from "@/ui/Paragraph";
 
 export default function Home() {
   return (
-    <main className="bg-gray-800">
+    <main className="">
       <Heading size="lg">Similaroty Api</Heading>
       <Paragraph className="my-4" size="default">
         Hello World!
